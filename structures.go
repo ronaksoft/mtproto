a29c059122ec21fc3487be38bd6a00f40123dba3
Ehsan Noureddin Moosa <ehsan@ronaksoft.com>
2017-08-27 23:39:08 +0430

    change structures

--- structures.go
+++ structures.go
@@ -69,16 +69,16 @@ func (d *Dialog) GetInputPeer() TL {
 	switch d.Type {
 	case DIALOG_TYPE_CHAT:
 		return TL_inputPeerChat{
-			chat_id: d.PeerID,
+			Chat_id: d.PeerID,
 		}
 	case DIALOG_TYPE_CHANNEL:
 		return TL_inputPeerChannel{
-			channel_id:  d.PeerID,
-			access_hash: d.PeerAccessHash,
+			Channel_id:  d.PeerID,
+			Access_hash: d.PeerAccessHash,
 		}
 	case DIALOG_TYPE_USER:
 		return TL_inputPeerUser{
-			user_id: d.PeerID,
+			User_id: d.PeerID,
 		}
 	default:
 		return nil
@@ -135,8 +135,8 @@ type Document struct {
 
 func (d *Document) GetInputFileLocation() TL_inputDocumentFileLocation {
 	return TL_inputDocumentFileLocation{
-		id:          d.ID,
-		access_hash: d.AccessHash,
+		Id:          d.ID,
+		Access_hash: d.AccessHash,
 	}
 }
 func (p *PhotoSize) GetInputFileLocation() TL_inputFileLocation {
@@ -159,13 +159,13 @@ func NewPeer(in TL) (p *Peer) {
 	switch x := in.(type) {
 	case TL_peerChannel:
 		p.Type = PEER_TYPE_CHANNEL
-		p.ID = x.channel_id
+		p.ID = x.Channel_id
 	case TL_peerChat:
 		p.Type = PEER_TYPE_CHAT
-		p.ID = x.chat_id
+		p.ID = x.Chat_id
 	case TL_peerUser:
 		p.Type = PEER_TYPE_USER
-		p.ID = x.user_id
+		p.ID = x.User_id
 	}
 	return p
 }
@@ -175,10 +175,10 @@ func NewFileLocation(in TL) (fl *FileLocation) {
 	case TL_fileLocationUnavailable:
 		return nil
 	case TL_fileLocation:
-		fl.DC = x.dc_id
-		fl.LocalID = x.local_id
-		fl.Secret = x.secret
-		fl.VolumeID = x.volume_id
+		fl.DC = x.Dc_id
+		fl.LocalID = x.Local_id
+		fl.Secret = x.Secret
+		fl.VolumeID = x.Volume_id
 	}
 	return
 }
@@ -186,12 +186,12 @@ func NewPhoto(in TL) (photo *Photo) {
 	photo = new(Photo)
 	switch x := in.(type) {
 	case TL_photo:
-		photo.flags = x.flags
-		photo.ID = x.id
-		photo.AccessHash = x.access_hash
-		photo.Date = x.date
-		photo.Sizes = make([]*PhotoSize, 0, len(x.sizes))
-		for _, v := range x.sizes {
+		photo.flags = x.Flags
+		photo.ID = x.Id
+		photo.AccessHash = x.Access_hash
+		photo.Date = x.Date
+		photo.Sizes = make([]*PhotoSize, 0, len(x.Sizes))
+		for _, v := range x.Sizes {
 			photo.Sizes = append(photo.Sizes, NewPhotoSize(v))
 		}
 	default:
@@ -205,11 +205,11 @@ func NewPhotoSize(in TL) (ps *PhotoSize) {
 	case TL_photoSizeEmpty:
 		return nil
 	case TL_photoSize:
-		ps.Type = x._type
-		ps.Size = x.size
-		ps.Width = x.w
-		ps.Height = x.h
-		ps.Location = NewFileLocation(x.location)
+		ps.Type = x._Type
+		ps.Size = x.Size
+		ps.Width = x.W
+		ps.Height = x.H
+		ps.Location = NewFileLocation(x.Location)
 	}
 	return
 }
@@ -217,13 +217,13 @@ func NewDocument(in TL) (d *Document) {
 	d = new(Document)
 	switch x := in.(type) {
 	case TL_document:
-		d.ID = x.id
-		d.AccessHash = x.access_hash
-		d.Mimetype = x.mime_type
-		d.Date = x.date
-		d.DcID = x.dc_id
-		d.Size = x.size
-		d.Thumb = NewPhotoSize(x.thumb)
+		d.ID = x.Id
+		d.AccessHash = x.Access_hash
+		d.Mimetype = x.Mime_type
+		d.Date = x.Date
+		d.DcID = x.Dc_id
+		d.Size = x.Size
+		d.Thumb = NewPhotoSize(x.Thumb)
 		//TODO:: Document Attribute
 	default:
 		log.Println("NewDocument::", reflect.TypeOf(x).String())
@@ -233,22 +233,22 @@ func NewDocument(in TL) (d *Document) {
 func NewDialog(input TL) (d *Dialog) {
 	d = new(Dialog)
 	if dialog, ok := input.(TL_dialog); ok {
-		switch pt := dialog.peer.(type) {
+		switch pt := dialog.Peer.(type) {
 		case TL_peerChat:
 			d.Type = DIALOG_TYPE_CHAT
-			d.PeerID = pt.chat_id
+			d.PeerID = pt.Chat_id
 		case TL_peerUser:
 			d.Type = DIALOG_TYPE_USER
-			d.PeerID = pt.user_id
+			d.PeerID = pt.User_id
 		case TL_peerChannel:
 			d.Type = DIALOG_TYPE_CHANNEL
-			d.PeerID = pt.channel_id
+			d.PeerID = pt.Channel_id
 		default:
 			return nil
 		}
-		d.Pts = dialog.pts
-		d.TopMessageID = dialog.top_message
-		d.UnreadCount = dialog.unread_count
+		d.Pts = dialog.Pts
+		d.TopMessageID = dialog.Top_message
+		d.UnreadCount = dialog.Unread_count
 
 		return d
 	}
@@ -259,8 +259,8 @@ func NewContact(in TL) (contact *Contact) {
 	contact = new(Contact)
 	switch c := in.(type) {
 	case TL_contact:
-		contact.UserID = c.user_id
-		contact.Mutual = toBool(c.mutual)
+		contact.UserID = c.User_id
+		contact.Mutual = toBool(c.Mutual)
 	default:
 		log.Println("GetContact::Error::Invalid Type")
 		return nil
